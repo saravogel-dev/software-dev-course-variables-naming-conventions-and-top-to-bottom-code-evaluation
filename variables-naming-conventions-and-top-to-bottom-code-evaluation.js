f5f6e070575911005474917d10e0a2eef762d7e2
saravogel-dev <saravf.dev@gmail.com>
2026-09-17 20:57:06 -0500

    Refactor variables with descriptive names

--- variables-naming-conventions-and-top-to-bottom-code-evaluation.js
+++ variables-naming-conventions-and-top-to-bottom-code-evaluation.js
@@ -19,14 +19,25 @@ You will:
 
 Things to reflect on:
   - Why is it important to use meaningful variable names?
+    - To understand meaning behing each Code 
+    - To be able to communicate properly with collaborates that are working on the code.
+    - For future contributors.
+    - For the easier debugging process.
   - What are the common pitfalls to avoid when naming variables?
+    - avoid using names that are taken like "let"
+    - avoid using numbers, or single words or alphabets that don't mean anything.
+    - Try to follow the trend which is using the camelCase technique.
   - How do clear variable names benefit team collaboration?
-  
+    When collaborators read the codes, they proper naming can represent the values properly and it decreases the possible confusion. Futthermore, it helps with debugging.
+
 */
 
-let a = "Alice";
-let b = 5;
-let c = 20;
-let d = a + " bought " + b + " items for $" + c + ".";
+let customerName = "Alice";
+let numberOfItems = 5;
+let totalCost = 20;
+const storeName = "Freshmarket";
+const storeLocation = "St. Louis";
 
-console.log(d);
+let purchaseReport = customerName + " purchased " + numberOfItems + " items for $" + totalCost + " at " + storeName + " in " + storeLocation + ".";
+
+console.log(purchaseReport);
